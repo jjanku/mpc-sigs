56f08029ba4c353fe4d917622610bf61497c7313
Jakub Janků <janku.jakub.jj@gmail.com>
2022-07-30 12:08:30 +0200

    remove unused Group trait

--- src/protocol.rs
+++ src/protocol.rs
@@ -4,8 +4,3 @@ pub trait Protocol {
     fn update(&mut self, data: &[u8]) -> ProtocolResult<Vec<u8>>;
     fn output(&mut self) -> ProtocolResult<Vec<u8>>;
 }
-
-pub trait Group {
-    fn sign(&self) -> Box<dyn Protocol>;
-    fn to_bytes(&self) -> Vec<u8>;
-}
